11492c5d7ece59a11b80db5f882fc3f5dabc7e87
Daniel Friesel <derf@finalrewind.org>
2011-07-20 22:31:43 +0200

    Release v0.51

--- lib/App/Raps2.pm
+++ lib/App/Raps2.pm
@@ -12,7 +12,7 @@ use File::BaseDir qw(config_home data_home);
 use File::Path qw(make_path);
 use File::Slurp qw(slurp write_file);
 
-our $VERSION = '0.50';
+our $VERSION = '0.51';
 
 sub new {
 	my ( $class, %opt ) = @_;
@@ -299,7 +299,7 @@ B<App::Raps2> is the backend for B<raps2>, a simple commandline password safe.
 
 =head1 VERSION
 
-This manual documents App::Raps2 version 0.50
+This manual documents App::Raps2 version 0.51
 
 =head1 METHODS
 
--- lib/App/Raps2/UI.pm
+++ lib/App/Raps2/UI.pm
@@ -8,7 +8,7 @@ use Carp qw(cluck confess);
 use POSIX;
 use Term::ReadLine;
 
-our $VERSION = '0.50';
+our $VERSION = '0.51';
 
 sub new {
 	my ($obj) = @_;
@@ -132,7 +132,7 @@ App::Raps2::UI - App::Raps2 User Interface
 
 =head1 VERSION
 
-This manual documents B<App::Raps2::UI> version 0.50
+This manual documents B<App::Raps2::UI> version 0.51
 
 =head1 DESCRIPTION
 
